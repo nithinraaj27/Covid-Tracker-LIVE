ea5d5fb94c33808478fe4237c4d01e28ff7529e1
nithinraaj27 <75876026+nithinraaj27@users.noreply.github.com>
2021-02-19 20:30:42 +0700

    covid tracker LIVE

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Covid-19 Tracker'),
+            backgroundColor: Colors.red,
+        ),
+          body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: WebView(
+              javascriptMode:JavascriptMode.unrestricted,
+              initialUrl: 'https://www.bing.com/covid/local/india?form=C19ANS',
+            ),
+          ),
+     ),
+      )
+    );
+  }
+}
